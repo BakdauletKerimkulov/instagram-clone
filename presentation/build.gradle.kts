b7fb1e450408d06ab6e1b9a6c928a9aba6bb23db
BakdauletKerimkulov <kerimkulovb@internet.ru>
2025-06-10 16:27:09 +0500

    permissions has been added

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -94,6 +94,8 @@ dependencies {
     //библиотека от Google, оборачивающая ActivityResultContracts в удобные Compose-инструменты
     implementation("com.google.accompanist:accompanist-permissions:0.34.0-alpha")
 
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
